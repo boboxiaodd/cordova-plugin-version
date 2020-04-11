dedb4fe5c863b3a6eadb1f2113a2faae36e2f00c
boboxiaodd <iam386@gmail.com>
2020-04-11 15:06:30 +0800

    Update BBPVersion.swift

--- src/ios/BBPVersion.swift
+++ src/ios/BBPVersion.swift
@@ -1,1 +1,19 @@
-//ready
+import Foundation
+
+@objc(BBPVersion) 
+class BBPVersion : CDVPlugin {
+    @objc(get:)
+    func get(command: CDVInvokedUrlCommand){
+        let infoDic = Bundle.main.infoDictionary
+        let appVersion = infoDic?["CFBundleShortVersionString"]
+        let appBuildVersion = infoDic?["CFBundleVersion"]
+        let appName = infoDic?["CFBundleDisplayName"]
+        let bundle_id = infoDic?["CFBundleIdentifier"]
+        let json = ["version": appVersion! ,
+                    "build": appBuildVersion! ,
+                    "bundle_id": bundle_id!,
+                    "appname": appName! ] as [String:Any]
+        let pluginResult = CDVPluginResult (status: CDVCommandStatus_OK, messageAs: json)
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
+}
